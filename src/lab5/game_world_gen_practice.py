bb5524b1e1e186a5a5a1a7a7623200a6c3043e65
CaptainDemytri <plm5256@psu.edu>
2024-02-08 22:09:23 -0500

    "Lab 5 Changes"

--- src/lab5/game_world_gen_practice.py
+++ src/lab5/game_world_gen_practice.py
@@ -30,15 +30,75 @@ from lab2.cities_n_routes import get_randomly_spread_cities, get_routes
 
 ''' Create helper functions here '''
 
+#Blittable surface function
+def createblittablesurface(size):
+    landscape = get_landscape(size)
+    print("Created a landscape of size", landscape.shape)
+    return pygame.surfarray.make_surface(landscape[:, :, :3]) 
+
+#Cities function
+def get_randomly_spread_cities(size, n_cities):
+    """
+    > This function takes in the size of the map and the number of cities to be generated 
+    and returns a list of cities with their x and y coordinates. The cities are randomly spread
+    across the map.
+    
+    :param size: the size of the map as a tuple of 2 integers
+    :param n_cities: The number of cities to generate
+    :return: A list of tuples, each representing a city, with random x and y coordinates.
+    """
+    # Consider the condition where x size and y size are different
+
+    cities = []
+
+    for i in range(n_cities):
+
+        cities.append((random.randrange(0,size[0]), random.randrange(0, size[1])))
+
+    return cities
+
+# Routes function
+def get_routes(city_names):
+    """
+    It takes a list of cities and returns a list of all possible routes between those cities. 
+    Equivalently, all possible routes is just all the possible pairs of the cities. 
+    
+    :param cities: a list of city names
+    :return: A list of tuples representing all possible links between cities/ pairs of cities, 
+            each item in the list (a link) represents a route between two cities.
+    """
+
+    routes = []
+
+    for i in city_names:
+
+        for j in city_names:
+
+            if i != j:
+                routes.append((i,j))
+    
+    return routes
+
+def getCoords(i):
+    iRoute = routes[i]
+
+    startName = iRoute[0]
+    endName = iRoute[1]
+
+    startCoords = city_locations_dict[startName]
+    endCoords = city_locations_dict[endName]
+    
+    return [startCoords,endCoords]
+
 if __name__ == "__main__":
+
     pygame.init()
     size = width, height = 640, 480
     black = 1, 1, 1
 
     screen = pygame.display.set_mode(size)
-    landscape = get_landscape(size)
-    print("Created a landscape of size", landscape.shape)
-    pygame_surface = pygame.surfarray.make_surface(landscape[:, :, :3]) 
+
+    pygame_surface = createblittablesurface(size)
 
     city_names = ['Morkomasto', 'Morathrad', 'Eregailin', 'Corathrad', 'Eregarta',
                   'Numensari', 'Rhunkadi', 'Londathrad', 'Baernlad', 'Forthyr']
@@ -46,6 +106,8 @@ if __name__ == "__main__":
     routes = []
 
     ''' Setup cities and routes in here'''
+    city_locations = get_randomly_spread_cities(size, len(city_names))
+    routes = get_routes(city_names)
 
     city_locations_dict = {name: location for name, location in zip(city_names, city_locations)}
     random.shuffle(routes)
@@ -60,7 +122,19 @@ if __name__ == "__main__":
         screen.blit(pygame_surface, (0, 0))
 
         ''' draw cities '''
-
+        for i in city_locations:
+            pygame.draw.circle(screen, (220,250,230), i, 1)
         ''' draw first 10 routes '''
+        
+        iter = 0
+
+        for i in routes:
+            
+            coords = getCoords(iter)
+            start = coords[0]
+            end = coords[1]
+            iter += 1
+
+            pygame.draw.line(pygame_surface,(220,250,230),start,end)
 
         pygame.display.flip()
