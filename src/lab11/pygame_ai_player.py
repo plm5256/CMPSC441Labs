460d7f3288c0acc0e2dbf6e34628b818c1bc27ad
Peter <plm5256@psu.edu>
2024-03-24 21:24:55 -0400

    Lab 11 changes

--- src/lab11/pygame_ai_player.py
+++ src/lab11/pygame_ai_player.py
@@ -1,12 +1,36 @@
 """ Create PyGameAIPlayer class here"""
+import pygame
+from lab11.turn_combat import CombatPlayer
 
 
 class PyGameAIPlayer:
-    pass
+    def __init__(self) -> None:
+        pass
+
+    def selectAction(self, state):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+            if event.type == pygame.KEYDOWN:
+                if ord("0") <= event.key <= ord("9"):
+                    return event.key
+        return ord(str(state.current_city))  # Not a safe operation for >10 cities
 
 
 """ Create PyGameAICombatPlayer class here"""
 
 
 class PyGameAICombatPlayer:
-    pass
+    def __init__(self, name):
+        super().__init__(name)
+
+    def weapon_selecting_strategy(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key in [ord("s"), ord("a"), ord("f")]:
+                        choice = {ord("s"): 1, ord("a"): 2, ord("f"): 3}[event.key]
+                        self.weapon = choice - 1
+                        return self.weapon
