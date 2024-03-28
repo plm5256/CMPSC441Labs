e421581693165797741d4c833dd5ae53007f687a
Peter <plm5256@psu.edu>
2024-03-27 23:26:12 -0400

    Lab 12 Changes

--- src/lab12/episode.py
+++ src/lab12/episode.py
@@ -11,3 +11,39 @@ Note that observation/state is a tuple of the form (player1_health, player2_heal
 Action is simply the weapon selected by the player.
 Reward is the reward for the player for that turn.
 '''
+
+import pygame
+from pathlib import Path
+
+from lab11.sprite import Sprite
+from lab11.turn_combat import CombatPlayer, Combat
+from lab11.pygame_ai_player import PyGameAICombatPlayer
+from lab11.pygame_human_player import PyGameHumanCombatPlayer
+
+def run_episode(playerOne, playerTwo):
+    p1HP = playerOne.health
+    p2HP = playerTwo.health
+
+    p1WEAP = playerOne.weapon
+
+    #Create list for tuples
+    episode = []
+
+    while p1HP > 0 or p2HP > 0:
+
+        #Setup observation
+        observation = (p1HP, p2HP)
+
+        #Setup player weapon
+        action = p1WEAP
+
+        #Setup player reward
+        reward = playerOne.reward
+
+        #Setup turn
+        turn = (observation, action, reward)
+
+        #Add turn to episode
+        episode.append(turn)
+
+    return episode
